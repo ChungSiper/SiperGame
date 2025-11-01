b2a4d80bea3f8e06b11bce5a3eecc54c807384e4
Nagi Mukuchi <bakachan9x@gmail.com>
2025-11-01 21:23:41 +0700

    Kiến thức mới vè Inventory, ItemData, Delegate, Scripttable...

---
+++ Assets/Scripts/Inventory/ItemDataSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "Data/Item Data SO")]
+public class ItemDataSO : ScriptableObject
+{
+    public string Name;
+    public Sprite Image;
+    public string Desription;
+}
